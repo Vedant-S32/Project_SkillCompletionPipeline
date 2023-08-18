14b4bc123706d91ab1a68969d9538991ddb7e455
VedantS <122535961+FuzzyFox-32@users.noreply.github.com>
2023-08-18 19:15:39 +0530

    scp initial commit

---
+++ script/API_Keys(github).gd
@@ -0,0 +1,7 @@
+extends Node
+class_name C_APIKeys
+
+var U_CollectionName #FireStore Collection Name
+var U_YoutubeAPIKey #Youtube API Key
+var U_FireStoreAPIKey #FireStore API Key
+var U_FireStoreProjectid #FireStore Project ID
